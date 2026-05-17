ae56503426b94c5b0cadbfb13a74130704708474
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2026-05-17 11:29:08 -0700

    Identity hashing

--- frontend/src/lib/server/db/schema/replacement.ts
+++ frontend/src/lib/server/db/schema/replacement.ts
@@ -1,4 +1,4 @@
-import { index, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { blob, index, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { users } from "./user";
 import { relations } from "drizzle-orm";
 
@@ -11,9 +11,10 @@ export const replacements = sqliteTable(
 			.notNull(),
 		deadname: text({ length: 500 }).notNull(),
         email: text({ length: 500 }),
-        replacement: text({ length: 500 }).notNull()
+        replacement: text({ length: 500 }).notNull(),
+		hash: blob().notNull()
 	},
-	(table) => [index('replacements_user_id_idx').on(table.userId), index('replacements_deadname_idx').on(table.deadname), index('replacements_email_idx').on(table.email)]
+	(table) => [index('replacements_user_id_idx').on(table.userId), index('replacements_deadname_idx').on(table.deadname), index('replacements_email_idx').on(table.email), index('replacements_hash_idx').on(table.hash)]
 );
 
 export const replacementsRelations = relations(replacements, ({ one }) => ({
--- frontend/src/routes/dashboard/refresh-identity/+page.server.ts
+++ frontend/src/routes/dashboard/refresh-identity/+page.server.ts
@@ -2,9 +2,11 @@ import type { Actions, PageServerLoad } from './$types';
 import { superValidate, message } from 'sveltekit-superforms';
 import { zod4 } from 'sveltekit-superforms/adapters';
 import { getDb, table } from '@/server/db';
-import { and, eq } from 'drizzle-orm';
+import { and, eq, not } from 'drizzle-orm';
 import { identityCreateSchema } from '@/validators/identityEditValidator';
 import { error } from '@sveltejs/kit';
+import { sha256 } from '@oslojs/crypto/sha2';
+import { PUBLIC_GLOBAL_SALT } from '$env/static/public';
 
 export const load = (async ({ locals, platform }) => {
 	const { user } = await locals.auth();
@@ -52,10 +54,11 @@ export const actions: Actions = {
 				deadname: true,
 				email: true
 			},
-			where: and(eq(table.replacements.deadname, data.deadname), data.email ? eq(table.replacements.email, data.email) : undefined)
+			where: and(eq(table.replacements.deadname, data.deadname), data.email ? eq(table.replacements.email, data.email) : undefined, not(eq(table.replacements.userId, user.id)))
 		});
-
 		if (existing) error(400, "That name already exists in our records and may cause conflicts when replacing");
+
+		const hash = sha256(new TextEncoder().encode(data.deadname+"_"+PUBLIC_GLOBAL_SALT))
 
 		const userExisting = await db.query.replacements.findFirst({
 			columns: {
@@ -65,14 +68,16 @@ export const actions: Actions = {
 		})
 
 		if (userExisting) await db.update(table.replacements).set({
-			...data
+			...data,
+			hash
 		}).where(eq(table.replacements.id, userExisting.id));
 		else await db
 			.insert(table.replacements)
 			.values({
 				id: crypto.randomUUID(),
 				userId: user.id,
-				...data
+				...data,
+				hash
 			});
 
 		return message(form, { type: 'success', text: 'Identity updated successfully!' });
